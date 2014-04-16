465695edbd979ab43832ba4d987f56ed29329be2
Adam Pearson <ampearson@gmail.com>
2014-04-15 22:44:00 -0700

    Add mopidy configuration service
    Allows for adding "?server=<hostname>" to url string for developing
    against a remote mopidy server.

--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,16 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+var services = angular.module('myApp.services', []);
+services.value('version', '0.1');
+
+services.factory('mopidyConfiguration', ['$location', function(location){
+    var config = {};
+    var serverName = location.search().server;
+
+    if(serverName){
+        config.webSocketUrl = "ws://" + serverName + ":6680/mopidy/ws";
+    }
+
+    return config;
+}]);
--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -3,12 +3,41 @@
 /* jasmine specs for services go here */
 
 describe('service', function() {
-  beforeEach(module('myApp.services'));
+    beforeEach(module('myApp.services'));
 
 
-  describe('version', function() {
-    it('should return current version', inject(function(version) {
-      expect(version).toEqual('0.1');
-    }));
-  });
+    describe('version', function() {
+        it('should return current version', inject(function(version) {
+            expect(version).toEqual('0.1');
+        }));
+    });
+
+    describe('mopidyConfiguration', function(){
+        var mock, mopidyConfiguration;
+
+        describe('when when server is in the location search', function(){
+            beforeEach(function(){
+                mock = {
+                    search: function(){
+                        return { server: "fooserver" };
+                    }
+                };
+
+                module(function($provide) {
+                    $provide.value('$location', mock);
+                });
+            });
+
+            it('should return the server as ws address', inject(function(mopidyConfiguration){
+                var address = mopidyConfiguration.webSocketUrl;
+                expect(address).toEqual("ws://fooserver:6680/mopidy/ws");
+            }));
+        });
+
+        describe('when the server is not specified', function(){
+            it('should not return a webSocketUrl value in the config', inject(function(mopidyConfiguration){
+                expect(mopidyConfiguration.webSocketUrl).not.toBeDefined();
+            }));
+        });
+    })
 });
