e52ce14cd83929d5fde9bbe797b2b21978dbc352
Adam Pearson <adam@substantial.com>
2014-04-16 10:37:05 -0700

    s/myApp/qlab

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
+angular.module('qlab.controllers', [])
   .controller('MyCtrl1', [function() {
 
   }])
--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -3,7 +3,7 @@
 /* jasmine specs for filters go here */
 
 describe('filter', function() {
-  beforeEach(module('myApp.filters'));
+  beforeEach(module('qlab.filters'));
 
 
   describe('interpolate', function() {
