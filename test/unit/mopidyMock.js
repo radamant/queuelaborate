85fbe4cb78923624b9a6192366daedd320606d43
Adam Pearson <ampearson@gmail.com>
2014-04-19 14:38:05 -0700

    Clean up stubs

--- test/unit/mopidyMock.js
+++ test/unit/mopidyMock.js
@@ -1,19 +1,19 @@
 var Mopidy = function(settings){
     var eventStubs = {}
+
     this.on = function(name, fn){
         if(eventStubs[name])
             eventStubs[name] = fn
     }
-    this.tracklist = {
-        setConsume: jasmine.createSpy("setConsume()")
-    }
 
-    this.playback = {
-        play: jasmine.createSpy("play()"),
-        pause: jasmine.createSpy("pause()"),
-        next: jasmine.createSpy("next()"),
-        previous: jasmine.createSpy("previous()")
-    }
+    this.tracklist = jasmine.createSpyObj(
+        'tracklist', ['setConsume']
+    );
+
+    this.playback = jasmine.createSpyObj(
+        'playback',
+        ['play', 'pause', 'next', 'previous']
+    );
 
     this.stubEvent = function(name){
         eventStubs[name] = function(){};
